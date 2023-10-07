48987df7048de6b2a274ab652a7287533f40f488
Fabio <fabioheiji@gmail.com>
2023-10-07 00:12:52 -0300

    use gunicorn to run django

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,20 +17,20 @@ python3 manage.py migrate
 # echo "${0}: collecting statics."
 
 echo "upload server."
-python3 manage.py runserver 0:8000
+# python3 manage.py runserver 0:8000
 
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-# cp -rv static/* static_shared/
+cp -rv static/* static_shared/
 
-# gunicorn yourapp.wsgi:application \
-#     --env DJANGO_SETTINGS_MODULE=yourapp.production_settings \
-#     --name yourapp \
-#     --bind 0.0.0.0:8000 \
-#     --timeout 600 \
-#     --workers 4 \
-#     --log-level=info \
-#     --reload
+gunicorn yourapp.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=yourapp.production_settings \
+    --name yourapp \
+    --bind 0.0.0.0:8000 \
+    --timeout 600 \
+    --workers 4 \
+    --log-level=info \
+    --reload
 function Error()
 {
     echo "Error occur at line $1"
